e679d49baaaecccb04c219de94b017f1a862568a
Poza Radu <poza.radu@gmail.com>
2022-04-15 12:43:06 +0300

    created stack nav inside the container in app.js

--- CommunitimUI/App.js
+++ CommunitimUI/App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
 
 export default function App() {
   return (
-   
+   <NavigationContainer>
+     <Stack.Navigator>
+       
+     </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
