761ec7e870d17f1ad04478fd1e2aa780241ccdba
Poza Radu <poza.radu@gmail.com>
2022-04-15 12:40:40 +0300

    Created default homepage

--- CommunitimUI/App.js
+++ CommunitimUI/App.js
@@ -1,38 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-function ScreenA(){
-  return(
-    <View>
-      <Text>
-        Screen A
-      </Text>
-    </View>
-  )
-}
+
 export default function App() {
   return (
-   <NavigationContainer>
-      <StackNavigator>
-        <Stack.Screen
-          name="Screen_A"
-          component={ScreenA}
-        />
-      </StackNavigator>
-   </NavigationContainer>
+   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ CommunitimUI/src/screens/HomePage.js
@@ -0,0 +1,18 @@
+export default function HomePage(){
+    return(
+      <View>
+        <Text>
+          This is the HomePage.
+        </Text>
+      </View>
+    )
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
