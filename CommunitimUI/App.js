741890dba3da5015734ed979904a7e729987f732
Poza Radu <poza.radu@gmail.com>
2022-04-15 12:45:01 +0300

    added the homepage inside the stack nav

--- CommunitimUI/App.js
+++ CommunitimUI/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import HomePage from './src/screens/HomePage';
 const Stack = createStackNavigator();
 
 
@@ -10,7 +11,10 @@ export default function App() {
   return (
    <NavigationContainer>
      <Stack.Navigator>
-       
+       <Stack.Screen 
+        name="screen_home"
+        component = {HomePage}
+       />
      </Stack.Navigator>
    </NavigationContainer>
   );
