4b00ec0daee3d653abb9323c73351ba3b561ec0f
Poza Radu <poza.radu@gmail.com>
2022-04-17 14:14:17 +0300

    split the homepage into header and footer

--- CommunitimUI/App.js
+++ CommunitimUI/App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -10,7 +8,12 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
    <NavigationContainer>
-     <Stack.Navigator>
+     <Stack.Navigator screenOptions ={
+         {
+           headerShown:false
+         }
+       }>
+       
        <Stack.Screen 
         name="screen_home"
         component = {HomePage}
--- CommunitimUI/src/screens/HomePage.js
+++ CommunitimUI/src/screens/HomePage.js
@@ -7,15 +7,37 @@ export default function HomePage({navigation}){
         <Text>
           This is the HomePage.
         </Text>
+        <View style = {styles.header}>
+          <Text>
+            This is the header.
+          </Text>
+        </View>
+        <View style={styles.footer}>
+          <Text>
+            This is the footer.
+          </Text>
+        </View>
       </View>
+      
     )
   }
 
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
+      backgroundColor: '#9933ff',
+    },
+    header:{
+      flex:2,
       justifyContent: 'center',
+      alignItems:'center'
+    },
+    footer:{
+      flex:1,
+      backgroundColor: '#fff',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      paddingVertical:50,
+      paddingHorizontal:30
     },
   });
