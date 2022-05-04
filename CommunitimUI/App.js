8799e2c0c8247fda2acbcee0144f672438dc07f4
Poza Radu <poza.radu@gmail.com>
2022-05-05 01:35:20 +0300

    added functionality to the signup text from homepage

--- CommunitimUI/App.js
+++ CommunitimUI/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomePage from './src/screens/HomePage';
 import LoginPage from './src/screens/LoginPage';
+import SignupPage from './src/screens/SignupPage';
 const Stack = createStackNavigator();
 
 
@@ -23,6 +24,11 @@ export default function App() {
       <Stack.Screen 
          name="screen_login"
          component={LoginPage}
+       />
+
+       <Stack.Screen
+        name="screen_signup"
+        component={SignupPage}
        />
      </Stack.Navigator>
    </NavigationContainer>
