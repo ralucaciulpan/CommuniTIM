a5858d372d08648a8903c22b6e0cf5e5101e1a45
Poza Radu <poza.radu@gmail.com>
2022-05-05 01:24:42 +0300

    create signUp page

---
+++ CommunitimUI/src/screens/SignupPage.js
@@ -0,0 +1,11 @@
+import {StyleSheet,Text,View,Pressable,Image, Dimensions,Button} from 'react-native';
+import React from 'react';
+export default function SignupPage(){
+    return(
+        <View>
+            <Text>
+                This is the SignupPage.
+            </Text>
+        </View>
+    )
+}
