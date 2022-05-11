151bd9e9ebc25abe79aa348cd5be8234e49af0f3
Poza Radu <poza.radu@gmail.com>
2022-05-12 00:55:39 +0300

    added signup fields

--- CommunitimUI/src/screens/SignupPage.js
+++ CommunitimUI/src/screens/SignupPage.js
@@ -1,4 +1,4 @@
-import {StyleSheet,Text,View,Pressable,Image, Dimensions,Button,SafeAreaView} from 'react-native';
+import {StyleSheet,Text,View,Pressable,Image, Dimensions,Button,SafeAreaView,TextInput} from 'react-native';
 import React from 'react';
 export default function SignupPage(){
     return(
@@ -12,14 +12,38 @@ export default function SignupPage(){
             </SafeAreaView>
 
             <SafeAreaView style={styles.footer}>
+                <Text style={styles.welcome_text}>
+                    Bine ai venit pe CommuniTIM!
+                </Text>
+                <Text style = {styles.fieldText}>username:</Text>
+                <View style = {styles.usernameFieldContainer}>
+                    <TextInput
+                    style={styles.usernameField}
+                    placeholder='...'
+                    />
+                </View>
 
+                <Text style = {styles.fieldText}>password:</Text>
+                <View style = {styles.passwordFieldContainer}>
+                    <TextInput
+                    style={styles.passwordField}
+                    placeholder='...'
+                    />
+                </View>
+
+                <Text style = {styles.fieldText}>mail:</Text>
+                <View style = {styles.mailFieldContainer}>
+                    <TextInput
+                    style={styles.passwordField}
+                    placeholder='...'
+                    />
+                </View>
             </SafeAreaView>
         </View>
     )
 }
 
-const {height} = Dimensions.get('window');
-  const logo_height = height*0.28;
+  const windowWidth = Dimensions.get('window').width;
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -42,5 +66,52 @@ const {height} = Dimensions.get('window');
     },
     logo:{
         marginBottom:25
-    }
+    },
+    welcome_text:{
+        fontSize:30,
+        fontWeight:'bold',
+        marginBottom:30
+      },
+      usernameField:{
+        fontSize:20,
+      },
+      passwordField:{
+        fontSize:20
+      },
+      usernameFieldContainer:{
+          borderLeftWidth:3,
+          borderRightWidth:3,
+          borderTopWidth:3,
+          borderBottomWidth:3,
+          borderColor:'#696969',
+          width:windowWidth-50,
+          height:40,
+          marginBottom:25
+      },
+      passwordFieldContainer:{
+        borderLeftWidth:3,
+        borderRightWidth:3,
+        borderTopWidth:3,
+        borderBottomWidth:3,
+        borderColor:'#696969',
+        width:windowWidth-50,
+        height:40,
+        marginBottom:25
+    },
+    mailFieldContainer:{
+        borderLeftWidth:3,
+        borderRightWidth:3,
+        borderTopWidth:3,
+        borderBottomWidth:3,
+        borderColor:'#696969',
+        width:windowWidth-50,
+        height:40
+    },
+      fieldText:{
+          color:'#000',
+          fontSize:15,
+          fontWeight:'bold',
+          marginBottom:15,
+          alignSelf: 'flex-start'
+      }
 })
