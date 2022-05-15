f17656ac32aa036733a51ad37385bf0ecf26a9a4
Poza Radu <poza.radu@gmail.com>
2022-05-15 23:52:12 +0300

    added actionPage title

--- CommunitimUI/src/screens/ActionPage.js
+++ CommunitimUI/src/screens/ActionPage.js
@@ -2,8 +2,37 @@ import {StyleSheet,Text,View,Pressable,Image, Dimensions,Button,SafeAreaView,Tex
 import React from 'react';
 export default function ActionPage(){
     return(
-        <View>
-            <Text>This is the action page!</Text>
+        
+        <View style = {styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>
+                    Selectează o acțiune:
+                </Text>
+            </View>
+            <View style={styles.body}>
+
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+    },
+    header:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    headerText:{
+        fontWeight:'bold',
+        color:"#9933ff",
+        fontSize:25,
+        marginTop:30
+    },
+    body:{
+        flex:4,
+        backgroundColor:'#302',
+    }
+})
