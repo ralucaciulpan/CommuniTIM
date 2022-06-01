1270978ada53e62e6e7ea7c1f7329a602b32e69b
Poza Radu <poza.radu@gmail.com>
2022-06-01 21:35:13 +0300

    added the Problem model

---
+++ communitim/src/main/java/com/rr/communitim/model/Problem.java
@@ -0,0 +1,68 @@
+package com.rr.communitim.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="problems_table")
+public class Problem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String category;
+    private String subcategory;
+    private double longitude;
+    private double latitude;
+    public Problem(){
+
+    }
+    public Problem(String category, String subcategory, double longitude, double latitude) {
+        this.category = category;
+        this.subcategory = subcategory;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(String subcategory) {
+        this.subcategory = subcategory;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Problem{" +
+                "id=" + id +
+                ", category='" + category + '\'' +
+                ", subcategory='" + subcategory + '\'' +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                '}';
+    }
+}
